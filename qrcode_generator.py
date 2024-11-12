38fa3a121b0ecc19b47615c68b06a059ffa66f67
vicky-844 <victoria.ballach844@gmail.com>
2024-11-12 11:47:50 +0100

    commit message

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 
 #add a banner
-st.image("https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Djg5lzWz6hRrsg6qQk1G2AHaCP%26pid%3DApi&sp=1730801804Tae849f136249b7bb111a89966e95cd9c8e659fa4f2ffa2ed1e69372d126d836e")
+st.image("https://i.pinimg.com/736x/70/0f/44/700f44fc91b474c0ad943882685b174e.jpg")
 
 #add a title
 st.title("QR Code Generator")
@@ -23,7 +23,7 @@ url = st.text_input("Please enter the URL you want to encode   ⬇")
 #definition (CREATE QR-CODE)
 def generate_qrcode(url):
     qrcode = segno.make_qr(url)
-    qrcode.to_pil(scale=5,
+    qrcode.to_pil(scale=10,
                   dark="#672eb4").save("qrcode_gen.png")
 
 #DISPLAY QR-CODE (-> overwrites qr-codes; otherwise add time-stamp)
